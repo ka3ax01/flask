c5e8569e790d49134284b160ce4b2a1baf8008f4
Rabbani <73340081+ka3ax01@users.noreply.github.com>
2023-08-06 15:08:39 +0000

    tut14

---
+++ tutorial14.py
@@ -0,0 +1,30 @@
+'''tutorial 14:
+redirect and errors'''
+from flask import Flask, redirect, url_for, render_template, request, abort
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    '''executing log in'''
+    return render_template('session.html')
+
+@app.route('/login', methods = ['POST','GET'])
+def login():
+    '''
+    checking if we have right method and
+      then sending to success function
+    '''
+    if request.method == 'POST':
+        if request.form['username'] == 'admin':
+            return redirect(url_for('success'))
+        else:
+            abort(401)
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/success')
+def success() -> str:
+    '''
+    returns string messaging that everything is right
+    '''
+    return 'logged in succesfully'
