a319f531ce8f0562b7911123c19d23572d46891e
Rabbani <73340081+ka3ax01@users.noreply.github.com>
2023-08-09 13:18:50 +0000

    added tutorial16

---
+++ tutorial16.py
@@ -0,0 +1,20 @@
+from werkzeug import secure_filename
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect(url_for('upload'))
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+@app.route('/uploader', methods = ['GET','POST'])
+def uploader():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded succesfully'
+
+if __name__ == '__main__':
+    app.run(debug=True)
