95c81819b502140228073a955e82c74004a5c81a
Rabbani <73340081+ka3ax01@users.noreply.github.com>
2023-08-09 14:41:56 +0000

    updated tutorial16.py

--- tutorial16.py
+++ tutorial16.py
@@ -1,5 +1,5 @@
-from werkzeug import secure_filename
-from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
